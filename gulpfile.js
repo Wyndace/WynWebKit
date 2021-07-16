d9506219ee631464014f65031ea66af3d3fa3cf0
Wyndace (Васильев Александр) <wyndace@hotmail.com>
2021-07-17 02:53:27 +0500

    added building function

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,11 @@ const fileinclude = require('gulp-file-include');
 const svgSprite = require('gulp-svg-sprite');
 const ttf2woff = require('gulp-ttf2woff');
 const ttf2woff2 = require('gulp-ttf2woff2');
+const tinypng = require('gulp-tinypng-compress');
 const fs = require('fs');
 const del = require('del')
+
+let authKeys = JSON.parse(fs.readFileSync('./auth.json', 'utf-8'));
 
 const stylesPreBuilding = () => {
 	return src('./src/scss/**/*.scss')
@@ -21,11 +24,18 @@ const stylesPreBuilding = () => {
 		.pipe(rename({ suffix: '.min', }))
 		.pipe(autoprefixer({ cascade: false, }))
 		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./app/css/'))
+		.pipe(dest('./app/css'))
 		.pipe(browserSync.stream());
 };
+const stylesBuilding = () => {
+	return src('./src/scss/**/*.scss')
+		.pipe(sass({ outputStyle: 'compressed', }).on('error', notify.onError()))
+		.pipe(rename({ suffix: '.min', }))
+		.pipe(autoprefixer({ cascade: false, }))
+		.pipe(dest('./build/css'))
+};
 
-const htmlBuilding = () => {
+const htmlPreBuilding = () => {
 	return src('./src/index.html')
 		.pipe(fileinclude({
 			prefix: '@',
@@ -35,14 +45,38 @@ const htmlBuilding = () => {
 		.pipe(browserSync.stream());
 };
 
+const htmlBuilding = () => {
+	return src('./src/index.html')
+		.pipe(fileinclude({
+			prefix: '@',
+			basepath: '@file'
+		}))
+		.pipe(dest('./build'))
+};
+
+
 const imgPreBuilding = () => {
 	return src(['.src/img/**.png', './src/img/**.jpeg', './src/img/**.jpg'])
 		.pipe(dest('./app/img'));
 };
 
+const imgBuilding = () => {
+	
+	return src(['.src/img/**.png', './src/img/**.jpeg', './src/img/**.jpg'])
+		.pipe(tinypng({
+		key: `${authKeys.tinypng}`,
+		}))
+	.pipe(dest('./build/img'))
+}
+
 const videoPreBuilding = () => {
 	return src(['./src/video/**.mp4', './src/video/**.mpeg', './src/video/**.webm', './src/video/**.mpg', './src/video/**.avi', './src/video/**.mov'])
 		.pipe(dest('.app/video'));
+};
+
+const videoBuilding = () => {
+	return src(['./src/video/**.mp4', './src/video/**.mpeg', './src/video/**.webm', './src/video/**.mpg', './src/video/**.avi', './src/video/**.mov'])
+		.pipe(dest('.build/video'));
 };
 
 const resourcesPreBuilding = () => {
@@ -50,7 +84,13 @@ const resourcesPreBuilding = () => {
 		.pipe(dest('./app/resources'));
 }
 
-const svgToSprite = () => {
+const resourcesBuilding = () => {
+	return src('./src/resources/**')
+		.pipe(dest('./build/resources'));
+}
+
+
+const svgToSpritePreBuilding = () => {
 	return src('.src/img/**.svg')
 		.pipe((svgSprite({
 			mode: {
@@ -60,6 +100,17 @@ const svgToSprite = () => {
 			}
 		})))
 		.pipe(dest('/app/img'));
+};
+const svgToSpriteBuilding = () => {
+	return src('.src/img/**.svg')
+		.pipe((svgSprite({
+			mode: {
+				stack: {
+					sprite: "../sprite.svg"
+				}
+			}
+		})))
+		.pipe(dest('/build/img'));
 };
 
 const fontsPreBuilding = () => {
@@ -71,10 +122,20 @@ const fontsPreBuilding = () => {
 		.pipe(dest('./app/fonts'));
 }
 
+const fontsBuilding = () => {
+	src('./src/fonts/**.ttf')
+		.pipe(ttf2woff())
+		.pipe(dest('./build/fonts'));
+	return src('./src/fonts/**.ttf')
+		.pipe(ttf2woff2())
+		.pipe(dest('./build/fonts'));
+}
+
 let initAttr = () => {
 	let srcFonts = './src/scss/_fonts.scss';
 	let appFonts = './app/fonts/';
-	return [srcFonts, appFonts]
+	let buildFonts = './build/fonts/';
+	return [srcFonts, appFonts, buildFonts]
 }
 
 const cb = () => {}
@@ -100,13 +161,43 @@ const fontsStyle = (done) => {
 	done();
 }
 
+const fontsStyleBuilding = (done) => {
+	let pathArr = initAttr();
+	let file_content = fs.readFileSync(pathArr[0]);
+	fs.writeFile(pathArr[0], '', cb);
+	fs.readdir(pathArr[2], function (err, items) {
+		if (items) {
+			let c_fontname;
+			for (var i = 0; i < items.length; i++) {
+				let fontname = items[i].split('.');
+				fontname = fontname[0];
+				if (c_fontname != fontname) {
+					fs.appendFile(pathArr[0], `@include font-face("${fontname}", "${fontname}", 400);\r\n`, cb);
+				}
+				c_fontname = fontname;
+			}
+		}
+	})
+
+	done();
+}
+
 const scriptsPreBuilding = () => {
 		return src('./src/js/**')
 		.pipe(dest('./app/js'));
 }
 
+const scriptsBuilding = () => {
+		return src('./src/js/**')
+		.pipe(dest('./build/js'));
+}
+
 const cleaner = () => {
 	return del(['./app/*'])
+}
+
+const buildCleaner = () => {
+	return del(['./build/*'])
 }
 
 const globalWatching = () => {
@@ -117,11 +208,11 @@ const globalWatching = () => {
 	});
 
 	watch('./src/scss/**/*.scss', stylesPreBuilding);
-	watch('./src/index.html', htmlBuilding);
+	watch('./src/index.html', htmlPreBuilding);
 	watch('./src/img/**.jpg', imgPreBuilding);
 	watch('./src/img/**.jpeg', imgPreBuilding);
 	watch('./src/img/**.png', imgPreBuilding);
-	watch('./src/img/**.svg', svgToSprite);
+	watch('./src/img/**.svg', svgToSpritePreBuilding);
 	watch('./src/video/**.mp4', videoPreBuilding);
 	watch('./src/video/**.mpeg', videoPreBuilding);
 	watch('./src/video/**.webm', videoPreBuilding);
@@ -134,4 +225,6 @@ const globalWatching = () => {
 	watch('./src/js/**', scriptsPreBuilding);
 };
 
-exports.default = series(cleaner, parallel(htmlBuilding, fontsStyle, imgPreBuilding, svgToSprite, videoPreBuilding, resourcesPreBuilding, scriptsPreBuilding), fontsPreBuilding, stylesPreBuilding, globalWatching);
+exports.default = series(cleaner, parallel(htmlPreBuilding, fontsStyle, imgPreBuilding, svgToSpritePreBuilding, videoPreBuilding, resourcesPreBuilding, scriptsPreBuilding), fontsPreBuilding, stylesPreBuilding, globalWatching);
+
+exports.build = series(buildCleaner, parallel(htmlBuilding, fontsStyleBuilding, imgBuilding, svgToSpriteBuilding, videoBuilding, resourcesBuilding, scriptsBuilding), fontsBuilding, stylesBuilding);

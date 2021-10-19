8add286dd680862edc16ef351c61a00af9ab73a6
Alexander Vasilev (Wyndace [github.com/Wyndace]) <wyndace.av@ya.ru>
2021-10-19 05:38:32 +0500

    added modalWindowCreator.js

---
+++ src/js/functions/modalWindowCreator.js
@@ -0,0 +1,58 @@
+const modalClose = (modal, enableScroll = true) => {
+  modal.classList.add("_hidden");
+  if (enableScroll) {
+      scrollEnabling(document.querySelector("body"), true)
+    }
+};
+
+const modalOpen = (modal) => {
+  if (modal) {
+    const activeModal = document.querySelector("[data-modal-window]");
+    if (activeModal && !activeModal.classList.contains("_hidden")) {
+      modalClose(activeModal, false);
+    } else {
+      scrollDisabling(document.querySelector("body"), true);
+    }
+  }
+  modal.classList.remove("_hidden");
+  modal.addEventListener("click", function (e) {
+    if (!e.target.closest("[data-modal-area]")) {
+      modalClose(e.target.closest("[data-modal-window]"));
+    }
+  });
+};
+
+const modalWindows = document.querySelectorAll("[data-modal-window]");
+if (modalWindows.length > 0) {
+  for (let index = 0; index < modalWindows.length; index++) {
+    const modalWindow = modalWindows[index];
+    if (!modalWindow.classList.contains("_hidden")) {
+      modalWindow.classList.add("_hidden");
+    }
+  }
+}
+
+const modals = document.querySelectorAll("[data-modal]");
+if (modals.length > 0) {
+  for (let index = 0; index < modals.length; index++) {
+    const modal = modals[index];
+    modal.addEventListener("click", function (e) {
+      const modalName = modal.dataset.modal;
+      const currentModal = document.querySelector(modalName);
+      modalOpen(currentModal);
+      e.preventDefault();
+    });
+  }
+}
+
+const modalClosingItems = document.querySelectorAll("[data-close-modal]");
+if (modalClosingItems.length > 0) {
+  for (let index = 0; index < modalClosingItems.length; index++) {
+    const modalClosingItem = modalClosingItems[index];
+    modalClosingItem.addEventListener("click", function (e) {
+      console.log(modalClosingItem.closest("[data-modal-window]"));
+      modalClose(modalClosingItem.closest("[data-modal-window]"));
+      e.preventDefault();
+    });
+  }
+}
--- src/js/global.js
+++ src/js/global.js
@@ -8,9 +8,10 @@ fileInclude.include('./functions/_animation.js');
 
 
 fileInclude.include('./functions/smoothScrollTo.js'); // Плавная прокрутка до элемента. ТРЕБУЕТ _animation.js
-fileInclude.include('./functions/scrollDisabling.js'); // Полное отключение прокрутки у элемента.
+fileInclude.include('./functions/scrollDisabling.js'); // Полное отключение/включение прокрутки у элемента.
 fileInclude.include('./functions/spoilerCreator.js'); // Спойлеры и аккордионы. ТРЕБУЕТ _animation.js
 fileInclude.include('./functions/sendingOnPage.js'); // Проверка и отправка формы без перезагрузки страницы.
 fileInclude.include('./functions/phoneMask.js'); // Маска для телефона.
+fileInclude.include('./functions/modalWindowCreator.js'); // Модальные окна. ТРЕБУЕТ ScrollDisabling.js
 
 // ====================================================================================================================================================================

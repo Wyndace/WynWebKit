27d3374d8ecd86155b43e93b595169fe6fab4f6f
Alexander Vasilev (Wyndace [github.com/Wyndace]) <wyndace.av@ya.ru>
2021-10-19 05:43:55 +0500

    updated scrollDisabling function (added scrollEnabling)

--- src/js/functions/scrollDisabling.js
+++ src/js/functions/scrollDisabling.js
@@ -1,23 +1,70 @@
 // === Scroll disabling ===============================================================================================================================================
 
-const scrollDisabling = (element, positon) => {
-  if (!element.classList.contains('_scroll-disabled') && !element.classList.contains('_scroll-disabled_horizontal') && !element.classList.contains('_scroll-disabled_vertical')) {
-    if (positon == "verical") {
-      element.classList.add('_scroll-disabled_vertical');
-    } else if (positon == "horizontal") {
-      element.classList.add('_scroll-disabled_horizontal');
+const scrollDisabling = (element, fixed = false, position) => {
+  if (
+    !element.classList.contains("_scroll-disabled") &&
+    !element.classList.contains("_scroll-disabled_horizontal") &&
+    !element.classList.contains("_scroll-disabled_vertical")
+  ) {
+    if (fixed) {
+      if (element == document.body) {
+        paddingOffset = innerWidth - document.body.offsetWidth + "px";
+        document.body.style.paddingRight = paddingOffset;
+        if (scrollFixedElements.length > 0) {
+          for (let index = 0; index < scrollFixedElements.length; index++) {
+            const el = scrollFixedElements[index];
+            el.style.paddingRight = paddingOffset;
+            scrollDisabling(el);
+          }
+        }
+      }
+    }
+    if (position == "verical") {
+      element.classList.add("_scroll-disabled_vertical");
+    } else if (position == "horizontal") {
+      element.classList.add("_scroll-disabled_horizontal");
     } else {
-      element.classList.add('_scroll-disabled');
+      element.classList.add("_scroll-disabled");
     }
   }
 };
 
-const scrollDisablers = document.querySelectorAll('[data-scroll_disable]');
+const scrollEnabling = (element, fixed = false, position) => {
+  if (
+    element.classList.contains("_scroll-disabled") ||
+    element.classList.contains("_scroll-disabled_horizontal") ||
+    element.classList.contains("_scroll-disabled_vertical")
+  ) {
+    if (fixed) {
+      if (element == document.body) {
+        document.body.style.paddingRight = 0;
+        if (scrollFixedElements.length > 0) {
+          for (let index = 0; index < scrollFixedElements.length; index++) {
+            const el = scrollFixedElements[index];
+            el.style.paddingRight = 0;
+            scrollEnabling(el);
+          }
+        }
+      }
+    }
+    if (position == "verical") {
+      element.classList.remove("_scroll-disabled_vertical");
+    } else if (position == "horizontal") {
+      element.classList.remove("_scroll-disabled_horizontal");
+    } else {
+      element.classList.remove("_scroll-disabled");
+    }
+  }
+};
+
+const scrollDisablers = document.querySelectorAll("[data-scroll_disable]");
 if (scrollDisablers.length > 0) {
   for (scrollDisabler of scrollDisablers) {
     positon = scrollDisabler.dataset.scroll_disable;
     scrollDisabling(scrollDisabler, positon);
   }
 }
+
+const scrollFixedElements = document.querySelectorAll("[data-scroll-fixed]");
 
 // ====================================================================================================================================================================

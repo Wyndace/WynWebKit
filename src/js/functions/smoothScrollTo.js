a0ba80755174630a51c3c72f17636546a6cf18d5
Alexander Vasilev (Wyndace [github.com/Wyndace]) <wyndace.av@ya.ru>
2021-10-29 20:03:22 +0500

    updated and fixed smoothScrollTo.js

--- src/js/functions/smoothScrollTo.js
+++ src/js/functions/smoothScrollTo.js
@@ -1,32 +1,38 @@
 // === Smooth scroll to ===============================================================================================================================================
 
 const smoothScroll = (target, duration) => {
-  const topOffset = document.querySelector('.header').offsetHeight;
+  const topOffset = getComputedStyle(document.querySelector('.header')).position == 'fixed' || getComputedStyle(document.querySelector('.header')).position == 'sticky' ? document.querySelector('.header').offsetHeight : 0;
 
-  const targetPosition = target.getBoundingClientRect().top;
+  // console.log(topOffset)
+  console.log(target);
+
+  const targetPosition = target.getBoundingClientRect().top + window.scrollY;
 
   const startPositon = window.pageYOffset;
 
-  const distance = targetPosition - startPositon;
+  const distance = targetPosition - startPositon - topOffset;
 
   let startTime = null;
 
-  requestAnimationFrame(smoothScrollAnimation);
+  const SmoothScrollAnim = (currentTime) => smoothScrollAnimation(currentTime, distance, duration, startTime, startPositon);
+
+  requestAnimationFrame(SmoothScrollAnim);
 }
 
 const smoothScrollers = document.querySelectorAll('[data-scroll_to]');
 if (smoothScrollers.length > 0) {
-  for (smoothScroller of smoothScrollers) {
-    if (smoothScroller.dataset.data-scroll_to != '') {
+  smoothScrollers.forEach((smoothScroller) => {
+    if (smoothScroller.dataset.scroll_to != '') {
       smoothScroller.addEventListener('click', (e) => {
         e.preventDefault();
-        const target = document.querySelector(smoothScroller.dataset.data-scroll_to);
-        smoothScroll(target, 500);
+        const target = document.querySelector(smoothScroller.dataset.scroll_to);
+        smoothScroll(target, 1000);
+        console.log(target);
       });
     } else {
       console.log('You must type selector in data-scroll_to');
     }
-  }
+  });
 }
 
 // ====================================================================================================================================================================

bfb17e8a92243cf4f354360119f5ab62492e74f2
Alexander Vasilev (Wyndace [github.com/Wyndace]) <wyndace.av@ya.ru>
2021-10-30 07:11:00 +0500

    added parseBoolean function

--- src/js/functions/_functions.js
+++ src/js/functions/_functions.js
@@ -1,3 +1,11 @@
 const createMatch = (string, regex) => {
   return regex.test(string);
 };
+
+const parseBoolean = value => {
+  if (typeof value === "string") {
+    value = value.replace(/^\s+|\s+$/g, "").toLowerCase();
+    if (value === "true" || value === "false") return value === "true";
+  }
+  return;
+}

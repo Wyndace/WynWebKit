742025d8f93e081678e4649161410e528618ef6f
Alexander (Wyndace) <wyndace.av@ya.ru>
2021-12-31 15:55:37 +0500

    restructed tabCreator plugin

--- src/js/functions/tabsCreator.js
+++ src/js/functions/tabsCreator.js
@@ -1,23 +1,23 @@
-const tabOpen = (path) => {
-  const tabOpener = document.querySelector(`[data-tabpath="${path}"]`);
+ const tabOpen = (path) => {
+  const tabOpener = document.querySelector(`[data-tab_path="${path}"]`);
   const tab = document.querySelector(path);
   tabOpener.parentElement.classList.add("_active");
   // tabOpener.classList.add("_active");
   tab.classList.remove("_hidden");
 };
 
-const tabsOpeners = document.querySelectorAll("[data-tabpath]");
+const tabsOpeners = document.querySelectorAll("[data-tab_path]");
 if (tabsOpeners.length > 0) {
   for (let index = 0; index < tabsOpeners.length; index++) {
     const tabsOpener = tabsOpeners[index];
-    const tabName = tabsOpener.dataset.tabpath;
+    const tabName = tabsOpener.dataset.tab_path;
     console.log(tabName);
     tabsOpener.addEventListener("click", function (e) {
       e.preventDefault();
       tabsOpeners.forEach((el) => {
         el.parentElement.classList.remove("_active") &&
           el.classList.remove("_active");
-        document.querySelectorAll(el.dataset.tabpath).forEach((tab) => {
+        document.querySelectorAll(el.dataset.tab_path).forEach((tab) => {
           tab.classList.add("_hidden");
         });
       });

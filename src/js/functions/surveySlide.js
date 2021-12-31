47716007bf1739940160abfd0ddb7c9b9fbf20c6
Alexander (Wyndace) <wyndace.av@ya.ru>
2021-12-31 15:55:11 +0500

    restructed surveySlide plugin

--- src/js/functions/surveySlide.js
+++ src/js/functions/surveySlide.js
@@ -6,20 +6,17 @@ const data = [
     answer_alias: "quality",
     slideAnswers: [
       {
-        answer_image:
-          "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
+        answer_image: "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
         answer_title: "Супер",
         type: "radio",
       },
       {
-        answer_image:
-          "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
+        answer_image: "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
         answer_title: "Крутой",
         type: "radio",
       },
       {
-        answer_image:
-          "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
+        answer_image: "https://fakeimg.pl/280x293/5ce1e6/fff/?text=surveySlide&font=Intro",
         answer_title: "Лучше него, ничего не видел!",
         type: "radio",
       },
@@ -62,49 +59,10 @@ const surveySlideValid = (slide) => {
   let error = 0;
   if (slide.hasAttribute("data-require")) {
     const fields = Array.prototype.slice.call(slide.querySelectorAll("input"));
-    fields.push.apply(
-      fields,
-      Array.prototype.slice.call(slide.querySelectorAll("textarea"))
-    );
+    fields.push.apply(fields, Array.prototype.slice.call(slide.querySelectorAll("textarea")));
     console.log(fields);
-    fields.forEach(field => {
-    //   formRemoveError(field);
-    //   if (field.tagName == "INPUT") {
-    //     if (field.dataset.form_input_email != null) {
-    //       if (!validateEmail(field.value)) {
-    //         formAddError(field);
-    //         ++error;
-    //         console.log(`${(field, error)}`);
-    //       } else if (field.dataset.form_input_tel != null) {
-    //         if (!validatePhone(field)) {
-    //           formAddError(field);
-    //           ++error;
-    //           console.log(`${(field, error)}`);
-    //         }
-    //       } else if (
-    //         field.getAttribute("type") === "checkbox" &&
-    //         !field.checked
-    //       ) {
-    //         formAddError(field);
-    //         ++error;
-    //         console.log(`${(field, error)}`);
-    //       } else {
-    //         if (field.value === "") {
-    //           formAddError(field);
-    //           ++error;
-    //           console.log(`${(field, error)}`);
-    //         }
-    //       }
-    //     }
-    //   } else {
-    //     console.log(field.value)
-    //     if (field.value === "") {
-    //       formAddError(field);
-    //       ++error;
-    //       console.log(`${(field, error)}`);
-    //     }
-    //   }
-    field.setAttribute('data-require', '');
+    fields.forEach((field) => {
+      field.setAttribute("data-require", "");
     });
   }
   return error;
@@ -114,29 +72,9 @@ const answersTemplateCreator = (data) => {
   const answersHTML = data.slideAnswers.map((item) => {
     return `
       <label>
-      ${
-        item.answer_image
-          ? `<div class='img-wrapper'><img src="${item.answer_image}"></div>`
-          : ""
-      }
-        ${
-          item.type == "textarea"
-            ? `${
-                item.answer_title ? `<label>${item.answer_title}</label>` : ""
-              } <textarea name="${data.answer_alias}"></textarea>`
-            : item.type == "text" || item.type == "email" || item.type == "tel"
-            ? `<label>${item.answer_title}</label><input name="${data.answer_alias}" type="${item.type}">`
-            : `<input name="${data.answer_alias}" value="${item.answer_title}" type="${item.type}">`
-        }
-        ${
-          item.answer_title &&
-          item.type != "text" &&
-          item.type != "email" &&
-          item.type != "tel" &&
-          item.type != "textarea"
-            ? item.answer_title
-            : ""
-        }
+      ${item.answer_image ? `<div class='img-wrapper'><img src="${item.answer_image}"></div>` : ""}
+        ${item.type == "textarea" ? `${item.answer_title ? `<label>${item.answer_title}</label>` : ""} <textarea name="${data.answer_alias}"></textarea>` : item.type == "text" || item.type == "email" || item.type == "tel" ? `<label>${item.answer_title}</label><input name="${data.answer_alias}" type="${item.type}">` : `<input name="${data.answer_alias}" value="${item.answer_title}" type="${item.type}">`}
+        ${item.answer_title && item.type != "text" && item.type != "email" && item.type != "tel" && item.type != "textarea" ? item.answer_title : ""}
       </label>
     `;
   });
@@ -144,49 +82,37 @@ const answersTemplateCreator = (data) => {
   return answersHTML;
 };
 
-const slideTemplateCreator = (
-  slides,
-  currentSlide,
-  data,
-  dataLength,
-  options
-) => {
+const slideTemplateCreator = (slides, currentSlide, data, dataLength, options) => {
   const { buttonSkipText, buttonNextText, buttonSubmitText } = options;
   slides.forEach((slide, index) => {
     const { title, require } = data[index];
-    const slideAnswers = slide.querySelector("[data-surveyAnswers");
+    const slideAnswers = slide.querySelector("[data-survey_answers");
     if (slideAnswers) {
       slideAnswers.innerHTML = answersTemplateCreator(data[index]).join("");
       slide.querySelectorAll("[type='tel']").forEach((item) => {
         item.setAttribute("data-form_input_tel", "");
-        item.addEventListener('input', phoneMask);
+        item.addEventListener("input", phoneMask);
       });
       slide.querySelectorAll('[type="email"]').forEach((item) => {
         item.setAttribute("data-form_input_email", "");
       });
-      if (slide.querySelector('[type="radio"]'))
-        slide
-          .querySelector('[type="radio"]')
-          .setAttribute("checked", "checked");
-          if (require) {
-            const fields = Array.prototype.slice.call(slide.querySelectorAll("input"));
-            fields.push.apply(
-              fields,
-              Array.prototype.slice.call(slide.querySelectorAll("textarea"))
-            );
-            fields.forEach(field => {
-              field.setAttribute('data-form_require', '') 
-            });
-          }
+      if (slide.querySelector('[type="radio"]')) slide.querySelector('[type="radio"]').setAttribute("checked", "checked");
+      if (require) {
+        const fields = Array.prototype.slice.call(slide.querySelectorAll("input"));
+        fields.push.apply(fields, Array.prototype.slice.call(slide.querySelectorAll("textarea")));
+        fields.forEach((field) => {
+          field.setAttribute("data-form_require", "");
+        });
+      }
     }
 
-    const slideTitle = slide.querySelector("[data-surveyTitle]");
+    const slideTitle = slide.querySelector("[data-survey_title]");
 
     if (slideTitle) {
       slideTitle.innerHTML = title;
     }
 
-    const slideButton = slide.querySelector("[data-surveyButton]");
+    const slideButton = slide.querySelector("[data-survey_button]");
     if (slideButton) {
       if (currentSlide != dataLength) {
         if (require) {
@@ -200,21 +126,15 @@ const slideTemplateCreator = (
     }
 
     slide.classList.add("_hidden");
-    if (
-      slide.dataset.surveyslide != "" &&
-      parseInt(slide.dataset.surveyslide) === currentSlide
-    ) {
+    if (slide.dataset.survey_slide != "" && parseInt(slide.dataset.survey_slide) === currentSlide) {
       slide.classList.remove("_hidden");
     }
 
-    const counter = document.querySelector("[data-surveyNumbers]");
+    const counter = document.querySelector("[data-survey_numbers]");
     if (counter) {
-      if (counter.dataset.surveynumbers != "")
-        counter.innerHTML = counter.dataset.surveynumbers
-          .replace("current", currentSlide)
-          .replace("all", dataLength);
+      if (counter.dataset.survey_numbers != "") counter.innerHTML = counter.dataset.survey_numbers.replace("current", currentSlide).replace("all", dataLength);
     }
-    const surveyEnd = document.querySelector("[data-surveyEnd]");
+    const surveyEnd = document.querySelector("[data-survey_end]");
 
     if (surveyEnd) {
       surveyEnd.classList.add("_hidden");
@@ -236,30 +156,21 @@ class surveySlide {
 
   init = () => {
     if (this.el) {
-      slideTemplateCreator(
-        this.el.querySelectorAll(`[data-surveySlide]`),
-        this.counter + 1,
-        this.data,
-        this.dataLength,
-        this.options
-      );
+      slideTemplateCreator(this.el.querySelectorAll(`[data-survey_slide]`), this.counter + 1, this.data, this.dataLength, this.options);
     }
   };
 
   events = () => {
     if (this.el) {
       this.el.addEventListener("click", (e) => {
-        const currentSlide = this.el.querySelector(
-          `[data-surveySlide="${this.data[this.counter].number}"]`
-        );
-        if (e.target == currentSlide.querySelector("[data-surveyButton]"))
-          this.click(e.target, currentSlide, this.options);
+        const currentSlide = this.el.querySelector(`[data-survey_slide="${this.data[this.counter].number}"]`);
+        if (e.target == currentSlide.querySelector("[data-survey_button]")) this.click(e.target, currentSlide, this.options);
       });
     }
   };
 
   click = (surveyButton, currentSlide, options) => {
-    // console.log(surveyButton, options);
+    // console.log(survey_button, options);
     const { buttonSkipText, buttonNextText, buttonSubmitText } = options;
     if (surveyButton.innerText == buttonNextText) {
       this.error = formValidate(currentSlide);
@@ -285,16 +196,10 @@ class surveySlide {
 
   send = (currentSlide) => {
     currentSlide.classList.add("_hidden");
-    currentSlide.parentNode
-      .querySelector("[data-surveyNumbers]")
-      .classList.add("_hidden");
-    currentSlide.parentNode
-      .querySelector("[data-surveyEnd]")
-      .classList.remove("_hidden");
-    currentSlide.parentNode
-      .querySelector("[data-surveyTitle]")
-      .classList.remove("_hidden");
-    currentSlide.querySelector("[data-surveyButton]").type = "submit";
+    currentSlide.parentNode.querySelector("[data-survey_numbers]").classList.add("_hidden");
+    currentSlide.parentNode.querySelector("[data-survey_end]").classList.remove("_hidden");
+    currentSlide.parentNode.querySelector("[data-survey_title]").classList.remove("_hidden");
+    currentSlide.querySelector("[data-survey_button]").type = "submit";
   };
 }
 

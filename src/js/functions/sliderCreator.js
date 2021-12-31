5c56ab6463408446b442e46c5dff8ad838faee0c
Alexander (Wyndace) <wyndace.av@ya.ru>
2021-12-31 15:53:31 +0500

    restructed sliderCreator

--- src/js/functions/sliderCreator.js
+++ src/js/functions/sliderCreator.js
@@ -1,17 +1,6 @@
-// import Swiper from 'swiper/bundle';
-
-// Swiper.use([Navigation, Pagination]);
 
 const sliders = document.querySelectorAll("[data-slider]");
-const SlidersButtons = document.querySelectorAll("[data-sliderButton]");
-
-// const initSliderButton = (btn) => {
-//   type = btn.dataset.sliderbutton;
-//   console.log(type);
-
-//   if (type === 'next') btn.addEventListener('click', () => btn.closest('[data-slider]').swiper.slideNext())
-//   else if (type === 'prev') btn.addEventListener('click', () => btn.closest('[data-slider]').swiper.slidePrev())
-// }
+const SlidersButtons = document.querySelectorAll("[data-slider_button]");
 
 if (sliders.length > 0) {   
   sliders.forEach((slider) => {
@@ -31,7 +20,6 @@ if (sliders.length > 0) {
       };
 
       const swiper = new Swiper(slider, {
-        // modules: [Navigation, Pagination],
         speed: sliderOptions.speed,
         initialSlide: sliderOptions.initialSlide,
         effect: sliderOptions.effect,
@@ -43,30 +31,24 @@ if (sliders.length > 0) {
 
         autoplay: sliderOptions.autoplay == true ? {delay: sliderOptions.apDelay} : sliderOptions.autoplay,
 
-        navigation: slider.querySelector('[data-sliderButton]') != null ? {
-          nextEl: "[data-sliderButton='next']",
-          prevEl: "[data-sliderButton='prev']",
+        navigation: slider.querySelector('[data-slider_button]') != null ? {
+          nextEl: "[data-slider_button='next']",
+          prevEl: "[data-slider_button='prev']",
         } : false,
         
-        pagination: slider.querySelector('[data-sliderPagination]') != null ? {
-          el: '[data-sliderPagination]',
-          type: slider.querySelector('[data-sliderPagination]').dataset.sliderpagination.split(', ')[0],
-          clickable: parseBoolean(slider.querySelector('[data-sliderPagination]').dataset.sliderpagination.split(', ')[1]),
-          dynamicBullets: parseBoolean(slider.querySelector('[data-sliderPagination]').dataset.sliderpagination.split(', ')[2]),
-          dynamicMainBullets: parseInt(slider.querySelector('[data-sliderPagination]').dataset.sliderpagination.split(', ')[3])
+        pagination: slider.querySelector('[data-slider_pagination]') != null ? {
+          el: '[data-slider_pagination]',
+          type: slider.querySelector('[data-slider_pagination]').dataset.slider_pagination.split(', ')[0],
+          clickable: parseBoolean(slider.querySelector('[data-slider_pagination]').dataset.slider_pagination.split(', ')[1]),
+          dynamicBullets: parseBoolean(slider.querySelector('[data-slider_pagination]').dataset.slider_pagination.split(', ')[2]),
+          dynamicMainBullets: parseInt(slider.querySelector('[data-slider_pagination]').dataset.slider_pagination.split(', ')[3])
         } : false,
 
-        scrollbar: slider.querySelector('[data-sliderScrollbar]') != null ? {
-          el: '[data-sliderScrollbar]',
-          draggable: parseBoolean(slider.querySelector('[data-sliderScrollbar]').dataset.sliderscrollbar)
+        scrollbar: slider.querySelector('[data-slider_scrollbar]') != null ? {
+          el: '[data-slider_scrollbar]',
+          draggable: parseBoolean(slider.querySelector('[data-slider_scrollbar]').dataset.slider_scrollbar)
         } : false,
       });
     } else new Swiper(slider);
   });
 }
-
-// SlidersButtons.forEach((btn) => {
-//   if (btn.dataset.sliderbutton != "") {
-//     initSliderButton(btn);
-//   }
-// });

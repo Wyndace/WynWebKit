37dda3baf664a95eaffc2a2089efc2f941cd10a8
Васильев Александр <wyndace.av@ya.ru>
2023-01-10 22:47:06 +0500

    added nuxt and plugins

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <SeoKit />
+    <!-- Generates screenshots for every page by default -->
+    <OgImageScreenshot />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ["nuxt-seo-kit"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-purgecss", "nuxt-svgo", "@nuxt/image-edge"],
+  runtimeConfig: {
+    trailingSlash: true,
+    indexable: false,
+    public: {
+      siteUrl: "http:/localhost:3000",
+      siteName: "Nuxt Site",
+      siteDescription: "Site base on Nuxt.",
+      language: "en-US",
+      titleSeparator: "|",
+    },
+  },
+})

0487ffcea204a39a06f1e0589317af67b9bd1ce2
Васильев Александр <wyndace.av@ya.ru>
2023-04-18 00:18:04 +0500

    chore: add flowbite's components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,6 @@
 import type { Config } from "tailwindcss"
 
-export default <Partial<Config>>{}
+export default <Partial<Config>>{
+  plugins: [require("flowbite")],
+  content: ["./node_modules/flowbite.{js,ts}"],
+}

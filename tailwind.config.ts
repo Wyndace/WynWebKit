7382eb6040c666ba8f9f51687e6524cb03ced58c
Васильев Александр <wyndace.av@ya.ru>
2023-05-06 20:08:28 +0500

    revert: delete flowbite's components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,3 @@
 import type { Config } from "tailwindcss"
 
-export default <Partial<Config>>{
-  plugins: [require("flowbite")],
-  content: ["./node_modules/flowbite.{js,ts}"],
-}
+export default <Partial<Config>>{}
